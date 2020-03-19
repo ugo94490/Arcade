fe8fa256b82c2797e03a9fa742734027ef744de1
Arthur Benard <abenardFix@arthurFix.localdomain>
2020-03-19 15:16:15 +0100

    [ADD] Pacman folder
    to brick branch to renard branc

---
+++ games/pacman/PacObject.cpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2020
+** OOP_arcade_2019
+** File description:
+** PacObject
+*/
+
+#include <iostream>
+#include "PacObject.hpp"
+
+PacObject::PacObject(float posx, float posy, char c)
+{
+    pos = std::pair<float, float>(posx, posy);
+    c == 'P' ? type = PLAYER : 0;
+    c == 'X' ? type = WALL : 0;
+    c == '*' ? type = STAR : 0;
+    c == 'O' ? type = DOOR : 0;
+    c == 'Q' ? type = GDOOR : 0;
+    c == ' ' ? type = SPACE : 0;
+    c == 'G' ? type = GHOST : 0;
+    appearance = type;
+}
+
+PacObject::~PacObject()
+{
+}
+
+char PacObject::getAppearance(void) const
+{
+    return appearance;
+}
+
+char PacObject::getType(void) const
+{
+    return type;
+}
+
+std::pair<float, float> PacObject::getPos(void) const
+{
+    return pos;
+}
+
+void PacObject::setPos(std::pair<float, float> pos)
+{
+    this->pos = pos;
+}
